1f4af27faea44d77e346eb8f58f08310453e9d02
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2019-04-11 10:44:50 +0200

    feat: enable status bar in finder

--- osx.sh
+++ osx.sh
@@ -52,6 +52,12 @@ defaults write NSGlobalDomain com.apple.trackpad.scaling -int 3
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
+# Enable path bar in Finder
+defaults write com.apple.finder ShowPathbar -int 1
+
+# Enable status bar in Finder
+defaults write com.apple.finder ShowStatusBar -int 1
+
 # Set the icon size of Dock items to 42 pixels
 defaults write com.apple.dock tilesize -int 42
 
